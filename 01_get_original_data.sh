58030851d45035f995160274322a0e74d8d3c507
George Austin <george@dyn-160-39-160-154.dyn.columbia.edu>
2019-05-03 16:02:18 -0400

    added stuff from the other repo

---
+++ 01_get_original_data.sh
@@ -0,0 +1,12 @@
+
+
+#Downloads the data into a zip file
+
+Curl -L0 http://tuvalu.santafe.edu/~aaronc/facultyhiring/replicationData_all.zip > replicationData_all.zip
+
+
+#touch and unzip the data
+
+touch replicationData_all.zip
+
+unzip replicationData_all.zip
